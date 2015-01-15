e1b9fb104626cc469d05ff47e45cc04c141b40af
CodedTiger <locke@frazierappcompany.com>
2015-01-15 17:20:56 -0500

    Beginning on Luhn's Algorithm.

---
+++ ValidCreditCardNumber/LuhnsAlgorithm.swift
@@ -0,0 +1,9 @@
+//
+//  LuhnsAlgorithm.swift
+//  ValidCreditCardNumber
+//
+//  Created by Locke Frazier on 1/15/15.
+//  Copyright (c) 2015 Frazier App Company, LLC. All rights reserved.
+//
+
+import Foundation
