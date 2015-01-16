bd75eede6a56d97642ab9fa249a39e2525a7892d
CodedTiger <locke@frazierappcompany.com>
2015-01-15 23:05:30 -0500

    Finished Luhn's Algorithm
    Takes in credit card number, return valid: true, invalid: false

--- ValidCreditCardNumber/LuhnsAlgorithm.swift
+++ ValidCreditCardNumber/LuhnsAlgorithm.swift
@@ -8,3 +8,37 @@
 
 import Foundation
 
+func validCard (var creditCardNumber: Int) -> Bool {
+    var numberOfDigits = 0
+    
+    var sumDigits = 0
+    
+    while creditCardNumber != 0 {
+        var tempNum = creditCardNumber % 10
+        var tempString: String;
+        
+        if numberOfDigits % 2 != 0 {
+            
+            tempNum *= 2
+            
+            if tempNum < 10 {
+                sumDigits += tempNum
+            } else {
+                let secondNum: Int = tempNum % 10
+                tempNum /= 10
+                let firstNum: Int = tempNum % 10
+                sumDigits += (firstNum + secondNum)
+            }
+        } else {
+            sumDigits += tempNum
+        }
+        creditCardNumber /= 10
+        numberOfDigits++
+    }
+    
+    if sumDigits % 10 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
